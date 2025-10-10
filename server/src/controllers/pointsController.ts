937ad7a6d2d86aa7b7471b713be31cdd4ecb4469
Victor <adsvictor@outlook.com>
2025-10-10 01:44:35 -0300

    ✨ feat: implement Items and Points controllers with routes

---
+++ server/src/controllers/pointsController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import knex from "../database/connection";
+
+export default class PointsController {
+  async create(request: Request, response: Response) {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } =
+      request.body;
+
+    const trx = await knex.transaction();
+
+    const point = {
+      name,
+      image: "image-fake",
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+    };
+
+    const insertedIds = await trx("points").insert(point);
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id,
+      };
+    });
+
+    await trx("point_items").insert(pointItems);
+    await trx.commit();
+
+    return response.json({ id: point_id, ...point });
+  }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import ItemsController from "./controllers/itemsController";
+import PointsController from "./controllers/pointsController";
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get("/", (request, response) => {
-  console.log("GET /");
-  return response.json({ message: "Hello World" });
-});
+routes.get("/items", itemsController.index);
+
+routes.post("/points", pointsController.create);
 
 export default routes;
