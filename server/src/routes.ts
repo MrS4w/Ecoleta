ddb4e23397d98043626e5e7fcd916b554d5caad8
Victor <adsvictor@outlook.com>
2025-10-10 02:01:19 -0300

    ✨ feat: implement index method in PointsController and update routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,7 +8,8 @@ const itemsController = new ItemsController();
 
 routes.get("/items", itemsController.index);
 
-routes.get("/points/:id", pointsController.show);
 routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
