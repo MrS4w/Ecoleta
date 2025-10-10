248518532a71db54fda20d458a7afb7c5a6e2be8
Victor <adsvictor@outlook.com>
2025-10-10 02:03:29 -0300

    ✨ feat: add CORS support to server and update package dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import path from "path";
 import routes from "./routes";
@@ -5,6 +6,8 @@ import routes from "./routes";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
