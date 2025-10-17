6fab9b96bdfd953ca1f8a9eea391d4094766e5ea
Victor <adsvictor@outlook.com>
2025-10-16 22:47:30 -0300

    ✨ feat: remove unused files and update index.html for Ecoleta branding

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,7 @@
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
